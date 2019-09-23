ddbbd76850f312dc19e51516eb2699889b8c334e
Sergey Kostin <sporeyo89@gmail.com>
2019-09-23 13:35:48 +0300

    new logo+popup+anchor for links

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,75 @@
-$('.responsive').slick({
-    dots: true,
-    infinite: false,
+
+
+$('.thanks__items').slick({
     speed: 300,
     slidesToShow: 4,
-    slidesToScroll: 4,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          infinite: true,
-          dots: true
+    slidesToScroll: 1,
+    variableWidth: true,
+    infinite: true,
+    responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
         }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-      // You can unslick at a given breakpoint now by adding:
-      // settings: "unslick"
-      // instead of a settings object
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
     ]
-  });
+});
+
+// Popap
+
+$(document).ready(function () {
+	PopUpHide();
+});
+
+function PopUpShow() {
+	$("#popap-question").show();
+};
+
+function PopUpHide() {
+	$("#popap-question").hide();
+};
+
+$('body').on('click', "#popap-question", function(e){
+    e.preventDefault();
+    $("#popap-question").hide();
+});
+
+$('#popap-question').children().click(function(e) {
+	return false;
+});
+
+// Якорь
+
+$(document).ready(function(){
+        $("#nav").on("click","a", function (event) {
+            //отменяем стандартную обработку нажатия по ссылке
+            event.preventDefault();
+            //забираем идентификатор бока с атрибута href
+            var id  = $(this).attr('href'),
+            //узнаем высоту от начала страницы до блока на который ссылается якорь
+                top = $(id).offset().top;
+            //анимируем переход на расстояние - top за 1500 мс
+            $('body,html').animate({scrollTop: top}, 1500);
+        });
+    });
+    
